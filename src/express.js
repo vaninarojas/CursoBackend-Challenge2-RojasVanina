182b450580f52dcb89906519e01b05ceab8b775f
vaninarojas <vanina_93@hotmail.com>
2024-04-15 22:37:36 -0300

    agrego try catch

--- src/express.js
+++ src/express.js
@@ -1,22 +1,29 @@
-import express from "express"
+import express from "express";
 import ProductManager from "./challenge3.js";
 
 const PORT = 3000;
 const app = express();
-const manager = new ProductManager("./src/products.json")
+const manager = new ProductManager("./src/products.json");
 
-app.get("/products", async (req, res) =>{
-    const limit = parseInt(req.query.limit) || 0;
-const products = await manager.getProducts(limit);
-res.send ({status: 1, payload: products});
-
+app.get("/products", async (req, res) => {
+    try {
+        const limit = parseInt(req.query.limit) || 0;
+        const products = await manager.getProducts(limit);
+        res.send({ status: 1, payload: products });
+    } catch (error) {
+        res.status(500).send({ status: 0, message: "Error al obtener los productos" });
+    }
 });
 
-app.get("/products/:pid", async (req, res) =>{
-const product = await manager.getProductById (req.params.pid);
-
-res.send ({status: 1, payload: product});
-
+app.get("/products/:pid", async (req, res) => {
+    try {
+        const product = await manager.getProductById(req.params.pid);
+        res.send({ status: 1, payload: product });
+    } catch (error) {
+        res.status(500).send({ status: 0, message: "Error al obtener el producto" });
+    }
 });
 
-app.listen(PORT, () => {console.log(`Servidor activo en puerto ${PORT}`);});
+app.listen(PORT, () => {
+    console.log(`Servidor activo en puerto ${PORT}`);
+});
